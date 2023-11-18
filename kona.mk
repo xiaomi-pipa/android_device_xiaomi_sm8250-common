6fe77b4a0532c5b4668834bb4810fe746ffd3a4b
Nolen Johnson <johnsonnolen@gmail.com>
2023-11-17 21:19:02 -0500

    sm8250-common: Opt out of GMS comms suite
    Change-Id: I7ebd0960ac9adad758ecb5ca1a3660f73447a9d1

--- kona.mk
+++ kona.mk
@@ -251,6 +251,9 @@ PRODUCT_PACKAGES += \
     android.hardware.health-service.qti_recovery
 endif
 
+# GMS
+WITH_GMS_COMMS_SUITE := false
+
 # HotwordEnrollement app permissions
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/permissions/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml
