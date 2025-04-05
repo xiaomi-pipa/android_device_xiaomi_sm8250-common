6d53a6d7f68fcad9dea28939a0fc15cd34b022a0
LuK1337 <priv.luk@gmail.com>
2025-04-05 22:18:31 +0200

    sm8250-common: libwfdservice_shim_v1 -> libwfdservice_shim
    Change-Id: Idf65dd61a2de80dfcbf72a8b75b6ca59234df573

--- extract-files.py
+++ extract-files.py
@@ -19,7 +19,7 @@ from extract_utils.fixups_lib import (
 
 blob_fixups: blob_fixups_user_type = {
     'system_ext/bin/wfdservice64': blob_fixup()
-        .add_needed('libwfdservice_shim_v1.so'),
+        .add_needed('libwfdservice_shim.so'),
     'system_ext/lib64/libwfdmmsrc_system.so': blob_fixup()
         .add_needed('libgui_shim.so'),
     'system_ext/lib64/libwfdnative.so': blob_fixup()
