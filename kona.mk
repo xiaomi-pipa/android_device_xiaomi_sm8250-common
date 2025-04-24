3b4763f99be00df4f2034bc50108065a94bc9cc6
CuriousNom <aryanbiswas7585@gmail.com>
2025-04-25 01:21:15 +0530

    sm8250-common: Supress few logspams
    * Until they are fixed

--- kona.mk
+++ kona.mk
@@ -310,6 +310,8 @@ SPAMMY_LOG_TAGS := \
      libsensor-parseRGB \
      libsensor-ssccalapi \
      sensors \
+     TextView \
+     PerformanceBooster \
      vendor.qti.hardware.display.composer-service \
      vendor.xiaomi.sensor.citsensorservice@2.0-service
 
