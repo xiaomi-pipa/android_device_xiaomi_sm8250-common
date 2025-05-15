d73ae4e53a05ec277e902c565bc4da596de23331
CuriousNom <aryanbiswas7585@gmail.com>
2025-05-15 11:58:59 +0530

    sm8250-common: Suppress few logspams

--- kona.mk
+++ kona.mk
@@ -298,17 +298,27 @@ $(call soong_config_set,libinit,vendor_init_lib,//$(LOCAL_PATH):init_xiaomi_kona
 
 # Logging
 SPAMMY_LOG_TAGS := \
+     AsyncOperation \
+     BackgroundInstallControlService \
+     BinderNative \
+     dxwi \
+     HWUI \
      MiStcImpl \
+     OtherActivityInputConsumer \
      SDM \
      SDM-histogram \
      SensorService \
      SRE \
+     WindowManager \
+     WindowManagerShell \
      WifiHAL \
      cnss-daemon \
      libcitsensorservice@2.0-impl \
      libsensor-displayalgo \
      libsensor-parseRGB \
      libsensor-ssccalapi \
+     libjingle \
+     StrictMode \
      sensors \
      TextView \
      PerformanceBooster \
